da3f84fb1ef66f689786ab43899acd2d1eda3eff
fazlam <fazlam.xworkz@gmail.com>
2022-09-14 19:29:57 +0530

    main

---
+++ CapitalByStateRunner.java
@@ -0,0 +1,29 @@
+class CapitalByStateRunner{
+	public static void main(String[]capitals)
+	{
+    String ref=CapitalByState.getCapitalByState("karnataka");
+	System.out.println(ref);
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
