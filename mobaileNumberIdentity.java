ba5f5b37d26e81b29f4645a65d5e0f130fe203d2
fazlam <fazlam.xworkz@gmail.com>
2022-09-14 19:30:53 +0530

    class

---
+++ mobaileNumberIdentity.java
@@ -0,0 +1,63 @@
+class mobaileNumberIdentity{
+	
+	static String getNameByMobile(double number)
+	
+	{
+	 System.out.println("enter getNameByMobile");
+	 if(number==8050153518L)
+			
+	{
+		System.out.println("my number");
+		return"number";
+	}
+	if(number==8197054535L)
+	{
+		System.out.println("fazlam");
+		return"fazlam";
+	}
+	if(number==9880209495L)
+	{
+		System.out.println("rudru");
+		return"rudru";
+	}
+	if(number==9667854367L)
+	{
+		System.out.println("vishwa");
+		return"vishwa";
+	}
+	if(number==9889977698L)
+	{
+	 System.out.println("veeru");
+		return"veeru";	
+	}
+	if(number==8970513009L)
+	{
+		System.out.println("sharan");
+		return"sharan";	
+	}
+	 if(number==8965463478L)
+	 {
+		System.out.println("prshant");
+		return"prshant";	  
+	 }
+	 if(number==9786543798L)
+	 {
+		System.out.println("sangu");
+		return"sangu";	    
+	 }
+	 if(number==9879876543L)
+	 {
+		System.out.println("boss");
+		return"boss";	   
+		 
+	 }
+	 if(number==8796547895L)
+	 {
+		System.out.println("sachin");
+		return"sachin";	   
+		 
+	 }
+	return null;
+	}
+	
+}
