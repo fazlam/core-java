9c251782c85fdbae308f270a3a346762f3b3bea0
fazlam <fazlam.xworkz@gmail.com>
2022-09-14 19:32:31 +0530

    main

---
+++ mobaileNumberIdentityRunner.java
@@ -0,0 +1,10 @@
+class mobaileNumberIdentityRunner{
+	
+	public static void main(String[]mobaile)
+	{
+	 String number=mobaileNumberIdentity.getNameByMobile(8970513009L);
+	
+	 System.out.println(number);
+	 return 
+	}
+}
