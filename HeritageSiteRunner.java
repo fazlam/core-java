576996f8166dcafa967d7ec91bfa5ac9f722e6b2
fazlam <fazlam.xworkz@gmail.com>
2022-09-23 08:09:20 +0530

    class

---
+++ HeritageSiteRunner.java
@@ -0,0 +1,53 @@
+class HeritageSiteRunner{
+	
+	
+	public static void main(String []value)
+	
+	{
+		System.out.println("HeritageSite in india");
+		
+		String ref=value[0];
+		System.out.println("Taj Mahal is in : "+ref);
+		
+		
+		String ref1=value[1];
+		System.out.println("Ajanta caves is in  : "+ref1);
+		
+		
+		String ref2=value[2];
+		System.out.println("Bodh Gaya is in : "+ref2);
+		
+		
+		String ref3=value[3];
+		System.out.println("Sun temple is in : "+ref3);
+		
+		
+		String ref4=value[4];
+		System.out.println("Red Fort is in : "+ref4);
+		
+		
+		String ref5=value[5];
+		System.out.println("Cholo Temple is in : "+ref5);
+		
+		
+		String ref6=value[6];
+		System.out.println(" Janter Manter is in: "+ref6);
+		
+		
+		String ref7=value[7];
+		System.out.println("The Pink City is : "+ref7);
+		
+		
+		String ref8=value[8];
+		System.out.println("Churches in: "+ref8);
+		
+		
+		String ref9=value[9];
+		System.out.println("Charminar is in  : "+ref9);
+		
+		
+		
+	}
+	
+	
+}
