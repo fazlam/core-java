1a456df1679a573985306e62aa3d4a9d9e2ac54e
fazlam <fazlam.xworkz@gmail.com>
2022-09-23 08:08:18 +0530

    class

---
+++ RiverRunner.java
@@ -0,0 +1,53 @@
+class RiverRunner{
+	
+	
+	public static void main(String []value)
+	
+	{
+		System.out.println("Information about  River");
+		
+		String ref=value[0];
+		System.out.println("River name : "+ref);
+		
+		
+		String ref1=value[1];
+		System.out.println("Country is  : "+ref1);
+		
+		
+		String ref2=value[2];
+		System.out.println("Region is : "+ref2);
+		
+		
+		String ref3=value[3];
+		System.out.println("Strat location is : "+ref3);
+		
+		
+		String ref4=value[4];
+		System.out.println("Discharge in  : "+ref4);
+		
+		
+		String ref5=value[5];
+		System.out.println("Total Elevation is : "+ref5);
+		
+		
+		String ref6=value[6];
+		System.out.println("Total lenght  is  : "+ref6);
+		
+		
+		String ref7=value[7];
+		System.out.println("Source is : "+ref7);
+		
+		
+		String ref8=value[8];
+		System.out.println("Islind is: "+ref8);
+		
+		
+		String ref9=value[9];
+		System.out.println("Mounths in  : "+ref9);
+		
+		
+		
+	}
+	
+	
+}
