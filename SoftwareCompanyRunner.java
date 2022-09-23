2a6503ac72a919395bf2d10de19e96e6a066474d
fazlam <fazlam.xworkz@gmail.com>
2022-09-23 08:11:07 +0530

    class

---
+++ SoftwareCompanyRunner.java
@@ -0,0 +1,53 @@
+class SoftwareCompanyRunner{
+	
+	
+	public static void main(String []value)
+	
+	{
+		System.out.println("Information of SoftwareCompany");
+		
+		String ref=value[0];
+		System.out.println("Company Name is : "+ref);
+		
+		
+		String ref1=value[1];
+		System.out.println("Industry is  : "+ref1);
+		
+		
+		String ref2=value[2];
+		System.out.println("Founder is  : "+ref2);
+		
+		
+		String ref3=value[3];
+		System.out.println("Headquaters : "+ref3);
+		
+		
+		String ref4=value[4];
+		System.out.println("CEO of company : "+ref4);
+		
+		
+		String ref5=value[5];
+		System.out.println("Products : "+ref5);
+		
+		
+		String ref6=value[6];
+		System.out.println(" Brand: "+ref6);
+		
+		
+		String ref7=value[7];
+		System.out.println("Net income : "+ref7);
+		
+		
+		String ref8=value[8];
+		System.out.println("Founded : "+ref8);
+		
+		
+		String ref9=value[9];
+		System.out.println("Location is : "+ref9);
+		
+		
+		
+	}
+	
+	
+}
