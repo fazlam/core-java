6ef157060a864eb78f891b51f8e3da95b652ce99
fazlam <fazlam.xworkz@gmail.com>
2022-09-23 08:05:59 +0530

    calss

---
+++ ChiefMinisterRunner.java
@@ -0,0 +1,53 @@
+class ChiefMinisterRunner{
+	
+	
+	public static void main(String []value)
+	
+	{
+		System.out.println("Information of Karnataka State");
+		
+		String ref=value[0];
+		System.out.println("ChiefMinister of Karnataka : "+ref);
+		
+		
+		String ref1=value[1];
+		System.out.println("Age is  : "+ref1);
+		
+		
+		String ref2=value[2];
+		System.out.println("Education : "+ref2);
+		
+		
+		String ref3=value[3];
+		System.out.println("Party : "+ref3);
+		
+		
+		String ref4=value[4];
+		System.out.println("Salary : "+ref4);
+		
+		
+		String ref5=value[5];
+		System.out.println("Set : "+ref5);
+		
+		
+		String ref6=value[6];
+		System.out.println(" Location is : "+ref6);
+		
+		
+		String ref7=value[7];
+		System.out.println("Wife Name is : "+ref7);
+		
+		
+		String ref8=value[8];
+		System.out.println("Son name is : "+ref8);
+		
+		
+		String ref9=value[9];
+		System.out.println("Daughter name is : "+ref9);
+		
+		
+		
+	}
+	
+	
+}
